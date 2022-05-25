37e1f408562d35b7c59bfeddaf477fe09b91d8c1
art3xa <a.kh.romanov@urfu.me>
2022-05-25 20:54:20 +0500

    Add release KV-Storage v1.1
    Signed-off-by: art3xa <A.Kh.Romanov@urfu.me>

---
+++ kvs_v1.1/server/server.py
@@ -0,0 +1,236 @@
+import ast
+import socket
+import threading
+
+conns = []
+stop = False
+
+
+def main():
+    """Запуск сервера - центра клиентов"""
+    host = socket.gethostname()
+    port = 5000
+    serv = socket.socket()
+    serv.bind((host, port))
+    serv.listen(100)
+    global conns
+
+    def sender():
+        """Отправляет команды клиентам"""
+        global stop
+        while not stop:
+            global conns
+            message = input(">>> ")
+            if message == 'exit':
+                stop = True
+                serv.close()
+                break
+            commands = message.split(maxsplit=2)
+            if commands[0] == 'add':
+                add(message)
+            elif commands[0] == 'get' or commands[0] == 'del':
+                get_and_delete(message)
+            elif commands[0] == 'exists':
+                exists(message)
+            elif commands[0] == 'save' or commands[0] == 'change' \
+                    or commands[0] == 'clone':
+                save_change_clone(message)
+            elif commands[0] == 'keys' or commands[0] == 'values':
+                keys_values(message)
+            elif commands[0] == 'all':
+                get_all(message)
+            elif commands[0] == 'size':
+                size(message)
+            else:
+                for i in range(len(conns)):
+                    conns[i].send(message.encode())
+                for j in range(len(conns)):
+                    try:
+                        data = conns[j].recv(1024)
+                        if data:
+                            print(data.decode())
+                    except socket.error as e:
+                        if e.errno == 10053:
+                            conns.pop(j)
+                            print("Подключено пользователй:", len(conns))
+                        else:
+                            raise
+
+    def acceptor():
+        """Постоянно принимает новых клиентов"""
+        global stop
+        while not stop:
+            global conns
+            conn, address = serv.accept()
+            conns.append(conn)
+            print(f"Connection from: {address}")
+            print("Подключено пользователй:", len(conns))
+
+    t1 = threading.Thread(target=acceptor)
+    t1.start()
+
+    storage_name = input("Please write modules name: ")
+    if storage_name:
+        for i in range(len(conns)):
+            conns[i].send(storage_name.encode())
+
+    for i in range(len(conns)):
+        try:
+            data = conns[i].recv(1024)
+            if data:
+                print(data.decode())
+        except socket.error as e:
+            if e.errno == 10053:
+                conns.pop(i)
+                print("Подключено пользователй:", len(conns))
+            else:
+                raise
+
+    t2 = threading.Thread(target=sender)
+    t2.start()
+
+    def add(message):
+        global conns
+        sizes = {}
+        for i in range(len(conns)):
+            conns[i].send('size'.encode())
+        for j in range(len(conns)):
+            try:
+                data = conns[j].recv(1024).decode()
+                if data:
+                    sizes[conns[j]] = int(data)
+            except socket.error as e:
+                if e.errno == 10053:
+                    conns.pop(j)
+                    print("Подключено пользователй:", len(conns))
+                else:
+                    raise
+        to_client = max(sizes, key=sizes.get)
+        to_client.send(message.encode())
+        dat = to_client.recv(1024)
+        print(dat.decode())
+
+    def get_and_delete(message):
+        global conns
+        gets = []
+        for i in range(len(conns)):
+            conns[i].send(message.encode())
+        for j in range(len(conns)):
+            try:
+                data = conns[j].recv(1024).decode()
+                if data:
+                    gets.append(data)
+            except socket.error as e:
+                if e.errno == 10053:
+                    conns.pop(j)
+                    print("Подключено пользователй:", len(conns))
+                else:
+                    raise
+        if not [i for i in gets if i != gets[0]]:
+            print(gets[0])
+        else:
+            for x in gets:
+                if not x.startswith('There is no data with the key'):
+                    print(x)
+
+    def exists(message):
+        global conns
+        exists = []
+        for i in range(len(conns)):
+            conns[i].send(message.encode())
+        for j in range(len(conns)):
+            try:
+                data = conns[j].recv(1024).decode()
+                if data:
+                    exists.append(data)
+            except socket.error as e:
+                if e.errno == 10053:
+                    conns.pop(j)
+                    print("Подключено пользователй:", len(conns))
+                else:
+                    raise
+        if not [i for i in exists if i != exists[0]]:
+            print(exists[0])
+        else:
+            for x in exists:
+                if not x.startswith('There is no key'):
+                    print(x)
+
+    def save_change_clone(message):
+        global conns
+        gets = []
+        for i in range(len(conns)):
+            conns[i].send(message.encode())
+        for j in range(len(conns)):
+            try:
+                data = conns[j].recv(1024).decode()
+                if data:
+                    gets.append(data)
+            except socket.error as e:
+                if e.errno == 10053:
+                    conns.pop(j)
+                    print("Подключено пользователй:", len(conns))
+                else:
+                    raise
+        if not [i for i in gets if i != gets[0]]:
+            print(gets[0])
+
+    def keys_values(message):
+        global conns
+        keys = []
+        for i in range(len(conns)):
+            conns[i].send(message.encode())
+        for j in range(len(conns)):
+            try:
+                data = conns[j].recv(1024).decode()
+                if data:
+                    new_data = data.split()
+                    keys = keys + new_data
+            except socket.error as e:
+                if e.errno == 10053:
+                    conns.pop(j)
+                    print("Подключено пользователй:", len(conns))
+                else:
+                    raise
+        print(keys)
+
+    def get_all(message):
+        global conns
+        all = {}
+        for i in range(len(conns)):
+            conns[i].send(message.encode())
+        for j in range(len(conns)):
+            try:
+                data = conns[j].recv(1024).decode()
+                if data:
+                    new_data = ast.literal_eval(data)
+                    all.update(new_data)
+            except socket.error as e:
+                if e.errno == 10053:
+                    conns.pop(j)
+                    print("Подключено пользователй:", len(conns))
+                else:
+                    raise
+        print(all)
+
+    def size(message):
+        global conns
+        size = 0
+        for i in range(len(conns)):
+            conns[i].send(message.encode())
+        for j in range(len(conns)):
+            try:
+                data = conns[j].recv(1024).decode()
+                if data:
+                    size = size + int(data)
+            except socket.error as e:
+                if e.errno == 10053:
+                    conns.pop(j)
+                    print("Подключено пользователй:", len(conns))
+                else:
+                    raise
+        print(size)
+
+
+if __name__ == '__main__':
+    main()
---
+++ kvs_v1.1/tests/test_storage.py
@@ -0,0 +1,148 @@
+import os
+import sys
+import unittest
+
+from kvstorage.storagemain import Storage
+
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                             os.path.pardir))
+
+
+class TestStorage(unittest.TestCase):
+
+    def test_init(self):
+        storage = Storage('test')
+        self.assertEqual(storage.data, {})
+        storage.exit()
+
+    def test_save(self):
+        storage = Storage('test')
+        storage.save()
+        self.assertEqual(storage.data, {})
+        storage.exit()
+
+    def test_add(self):
+        storage = Storage('test')
+        storage.add('1', '2')
+        storage.save()
+        self.assertEqual(storage.data, {"1": "2"})
+        storage.exit()
+
+    def test_get(self):
+        storage = Storage('test')
+        storage.add('2', '3')
+        self.assertEqual(storage.get('2'), '3')
+        storage.exit()
+
+    def test_delete(self):
+        storage = Storage('test')
+        storage.add('0', '1')
+        storage.delete('0')
+        self.assertEqual(storage.data, {})
+        storage.exit()
+
+    def test_exists_yes(self):
+        storage = Storage('test')
+        storage.add('0', '1')
+        self.assertEqual(
+            storage.exists('0'),
+            'The key "0" in data file "test"')
+        storage.exit()
+
+    def test_exists_no(self):
+        storage = Storage('test')
+        storage.add('5', '789')
+        self.assertEqual(storage.exists('1'),
+                         'There is no key "1" in data file "test"')
+        storage.exit()
+
+    def test_keys(self):
+        storage = Storage('test')
+        storage.add('1', '2')
+        storage.add('hi', 'world')
+        self.assertEqual(storage.keys(), '1 hi')
+        storage.exit()
+
+    def test_values(self):
+        storage = Storage('test')
+        storage.add('a', 'b')
+        storage.add('1', '3')
+        self.assertEqual(storage.values(), 'b 3')
+        storage.exit()
+
+    def test_get_file_size(self):
+        storage = Storage('test')
+        storage.add('1', '23')
+        self.assertEqual(
+            storage.get_file_size(),
+            89)
+        storage.exit()
+
+    def test_change_storage(self):
+        storage = Storage('test')
+        storage.add('a', 'b')
+        storage.change_storage('data')
+        self.assertEqual(storage.data, {})
+        storage.exit()
+
+    def test_exit(self):
+        storage = Storage('test')
+        self.assertEqual(storage.exit(), None)
+
+    def test_save_load(self):
+        storage = Storage('test')
+        storage.add('hi', 'python')
+        storage.save()
+        storage = Storage('test')
+        storage.save()
+        self.assertEqual(storage.data, {})
+
+
+class TestFails(unittest.TestCase):
+
+    def test_delete_exception(self):
+        storage = Storage('test')
+        storage.add('2', '1')
+        self.assertEqual(storage.delete('1'),
+                         'There is no data with the key 1 in data file test')
+        storage.exit()
+
+    def test_add_full_exception(self):
+        storage = Storage('test')
+        storage.add('hellowannaplayuno',
+                    'yesofcourceitsavalueforyourkeycheckitplease')
+        storage.add('hell0',
+                    'yesoryourkey1821809')
+        self.assertEqual(storage.add('1', '2'),
+                         f'There is no memory for your data in '
+                         f'data file {storage.name} now.\n'
+                         f'Delete something to add your data')
+        storage.exit()
+
+    def test_add_full(self):
+        storage = Storage('test')
+        storage.add('hellooprstylayuno',
+                    'yesofcourceitsavalueforyourkeycheckitplease')
+        storage.add('hell0',
+                    'yesoryourkey1821809')
+        self.assertEqual(
+            storage.get_file_size(),
+            0)
+        storage.exit()
+
+    def test_add_exception(self):
+        storage = Storage('test')
+        storage.add('1', '5')
+        self.assertEqual(storage.add('1', '2'), 'The key 1 is already used')
+        storage.exit()
+
+    def test_get_exception(self):
+        storage = Storage('test')
+        storage.add('1', '5')
+        self.assertEqual(storage.get('2'),
+                         'There is no data with the key 2 in data file test')
+        storage.exit()
+
+
+if __name__ == '__main__':
+    unittest.main()
